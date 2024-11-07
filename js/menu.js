a1933b47c87a552090041c918141d817a009692f
Welley Miguel <158380657+Dev-Miguel-Cafe98@users.noreply.github.com>
2024-11-07 16:28:42 -0300

    ajuste

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,6 @@ const botaoMenu = document.querySelector(".titulo-menu a");
 // Selecionando a lista de links do menu
 const listaDeLinks = document.querySelector(".links-menu");
 
-<<<<<<< HEAD
 // Ouvinte de evento para clicar no botão menu
 botaoMenu.addEventListener("click", function (event) {
     // Anulando o comportamento padrão do link
@@ -19,19 +18,4 @@ botaoMenu.addEventListener("click", function (event) {
         botaoMenu.innerHTML = " &equiv;"
     }
 
-=======
-// Ouvinte de evento para clicar no botao Menu
-botaoMenu.addEventListener("click", function(event){
-    // Anulando o comportamento padrão do link
-    event.preventDefault();
-    
-    // Alternar a classe aberto entre ativado/desativado
-    listaDeLinks.classList.toggle("aberto");
-
-    if(listaDeLinks.classList.contains("aberto")){
-        botaoMenu.innerHTML = "Fechar &times;"
-    } else {
-        botaoMenu.innerHTML = "Menu &equiv;"
-    }
->>>>>>> 47796049de7dbcbffb65bdd441a5a29f42468507
 });
